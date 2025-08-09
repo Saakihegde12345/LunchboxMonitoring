8b99e0791c576af7fe263069bf230e0afca16e77
Saaketh S Hegde <85739190+Saakihegde12345@users.noreply.github.com>
2025-08-09 16:36:19 +0530

    Update azure_iot.h

--- Lunchbox Monitoring_Wokwi/azure_iot.h
+++ Lunchbox Monitoring_Wokwi/azure_iot.h
@@ -10,30 +10,27 @@
 #define IOTHUB_HOSTNAME "LunchboxMonitoring.azure-devices.net"  // Replace with your IoT Hub name
 #define DEVICE_ID "lunchbox_esp32"
 
-// Primary connection string from Azure IoT Hub
-// Format: HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>
-#define CONNECTION_STRING "HostName=LunchboxMonitoring.azure-devices.net;DeviceId=lunchbox_esp32;SharedAccessKey=SharedAccessSignature sr=LunchboxMonitoring.azure-devices.net%2Fdevices%2Flunchbox_esp32_sim&sig=3zA%2FMzIDFaDc9%2B3Mu4m6wbAI55lm9xR1CKIIEzvns34%3D&se=1754822645"
-
 // MQTT Topics
-#define AZURE_IOT_HUB_TOPIC "devices/" DEVICE_ID "/messages/events/"
-#define AZURE_IOT_HUB_METHODS "$iothub/methods/POST/#"
+#define AZURE_IOT_HUB_TELEMETRY_TOPIC "devices/" DEVICE_ID "/messages/events/"
+#define AZURE_IOT_HUB_METHODS_POST "$iothub/methods/POST/#"
+#define AZURE_IOT_HUB_TWIN_UPDATE "$iothub/twin/res/#"
+#define AZURE_IOT_HUB_TWIN_GET "$iothub/twin/GET/?$rid=1"
+
 
 // SSL/TLS Configuration
 const char* root_ca = \
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n" \
-"... [truncated for brevity] ...\n" \
+"....\n" \
 "-----END CERTIFICATE-----";
 
 // Function declarations
-bool azureConnect();
+bool azureConnect(const char* sasToken);
 void azureDisconnect();
 bool azurePublish(const char* topic, const char* payload);
 void azureLoop();
 bool isAzureConnected();
+void mqttCallback(char* topic, byte* payload, unsigned int length);
 
-// Helper functions for SAS token generation
-String generateSasToken(String uri, String key, String policyName, int expiryInSeconds);
-String urlEncode(const char* msg);
 
 #endif
