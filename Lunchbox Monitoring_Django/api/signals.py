dc638890924811c958fc1c500b123229a3fc6909
Saaketh S Hegde <85739190+Saakihegde12345@users.noreply.github.com>
2025-08-21 22:16:34 +0530

    Delete Lunchbox Monitoring_Django/api directory

--- Lunchbox Monitoring_Django/api/signals.py
+++
@@ -1,5 +0,0 @@
-"""
-Signals for the API app.
-This file is intentionally left empty as we don't need any signals for the API app.
-The API app's apps.py imports this file, so we need to create it.
-"""
